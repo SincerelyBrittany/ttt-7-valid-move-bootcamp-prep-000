81be9435d35653f98cf1916ec0e4907c1da2df29
Brittany Russell <sincerelybr@gmail.com>
2020-02-28 03:04:06 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,58 @@
 # code your #valid_move? method here
 
 def valid_move?(board, position)
-	spot = position.to_i - 1
-	if !position_taken?(board, spot) && spot.between?(0,8)
-		true
-	else
-		false
-	end
+
+move = position.to_i
+test = move-1
+
+ 
+
+if position_taken?(board, test) == false && test.between?(0, 8)
+
+true
+
+else
+
+false
+
 end
+
+end
+
+ 
+
+ 
+
+ 
+
+def position_taken?(board, position)
+
+ 
+
+if board[position] == ” ” || board[position] == “” || board[position] == nil
+
+false
+
+else board[position] == “X” || board[position] == “O”
+
+true
+
+end
+
+ 
+
+end
+
+
+
+# def valid_move?(board, position)
+# 	spot = position.to_i - 1
+# 	if !position_taken?(board, spot) && spot.between?(0,8)
+# 		true
+# 	else
+# 		false
+# 	end
+# end
 
 
 # def valid_move?(board,index)
@@ -28,10 +73,12 @@ end
 # false && index.between?(0,8) == true
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "O"
-    true
-  else board[index] == "" || board[index] == " " || board[index] == nil
-    false
-  end
-end
+
+
+# def position_taken?(board, index)
+#   if board[index] == "X" || board[index] == "O"
+#     true
+#   else board[index] == "" || board[index] == " " || board[index] == nil
+#     false
+#   end
+# end
